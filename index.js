a4c91f7692a61cfed254e8a20105486c73d81ad4
faridho <faridho45@gmail.com>
2020-06-10 22:07:44 +0700

    add env

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/webhook', (req, res) => {
     if (mode && token) {
 
         // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+        if (mode === 'subscribe' && token === process.env.VERIFICATION_TOKEN) {
 
             // Responds with the challenge token from the request
             console.log('WEBHOOK_VERIFIED');
