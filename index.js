e52b1b03c578b48cae743e9f69efed307cda0fae
faridho <faridho45@gmail.com>
2020-06-11 22:54:43 +0700

    update all

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 'use strict';
 
-// Imports dependencies and set up http server
 const
     express = require('express'),
     bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json()), // creates express http server
+    app = express().use(bodyParser.json()), 
     request = require('request');
 
-app.get("/", function (req, res) {
+app.get("/", res => {
     res.send("Deployed!");
 });
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-// Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
-
     let body = req.body;
-
-    // Checks this is an event from a page subscription
+    
     if (body.object === 'page') {
-
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function (entry) {
-
-            // Gets the body of the webhook event
+        body.entry.forEach(entry => {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
 
-
-            // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
 
@@ -43,21 +31,14 @@ app.post('/webhook', (req, res) => {
 
         });
 
-        // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
     } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
 
 });
 
-// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    //let VERIFY_TOKEN = "thecoffetoken"
-
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -80,7 +61,6 @@ app.get('/webhook', (req, res) => {
     }
 });
 
-// Handles messages events
 function handleMessage(sender_psid, received_message) {
     let response;
 
@@ -125,7 +105,6 @@ function handleMessage(sender_psid, received_message) {
     callSendAPI(sender_psid, response);
 }
 
-// Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
     let response;
 
@@ -142,7 +121,6 @@ function handlePostback(sender_psid, received_postback) {
     callSendAPI(sender_psid, response);
 }
 
-// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
