8f67944b0c51aa7515584258034f44713abc88b4
Faridho <faridho@zeepos.com>
2020-08-02 15:04:28 +0700

    video

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ function handlePostback(sender_psid, received_postback) {
         //'tix': resources.tix
     }
 
-
     if (payload === "menu") {
         response = postbackData[payload]
     } else {
@@ -88,33 +87,32 @@ function handlePostback(sender_psid, received_postback) {
                 }
             }
         }
+    }
+    
 
+    callSendAPI(sender_psid, response);
+}
 
-
-
-        callSendAPI(sender_psid, response);
+function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
     }
 
-    function callSendAPI(sender_psid, response) {
-        let request_body = {
-            "recipient": {
-                "id": sender_psid
-            },
-            "message": response
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
         }
-
-        request({
-            "uri": "https://graph.facebook.com/v2.6/me/messages",
-            "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-            "method": "POST",
-            "json": request_body
-        }, (err, res, body) => {
-            if (!err) {
-                console.log('message sent!')
-            } else {
-                console.error("Unable to send message:" + err);
-            }
-        });
-    }
+    });
+}
 
     app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
