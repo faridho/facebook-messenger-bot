fe39c98d90ff27e87cfeb0ae861da481378b10d1
Faridho <faridho@zeepos.com>
2020-08-02 15:00:41 +0700

    video

--- index.js
+++ index.js
@@ -70,32 +70,51 @@ function handlePostback(sender_psid, received_postback) {
         //'tix': resources.tix
     }
 
-    
-    response = postbackData[payload]
 
-    callSendAPI(sender_psid, response);
-}
+    if (payload === "menu") {
+        response = postbackData[payload]
+    } else {
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "media",
+                    "elements": [
+                        {
+                            "media_type": "video",
+                            "url": "https://business.facebook.com/103593567765590/videos/630146130875938/"
+                        }
+                    ]
+                }
+            }
+        }
 
-function callSendAPI(sender_psid, response) {
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
+
+
+
+        callSendAPI(sender_psid, response);
     }
 
-    request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
+    function callSendAPI(sender_psid, response) {
+        let request_body = {
+            "recipient": {
+                "id": sender_psid
+            },
+            "message": response
         }
-    });
-}
 
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+        request({
+            "uri": "https://graph.facebook.com/v2.6/me/messages",
+            "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+            "method": "POST",
+            "json": request_body
+        }, (err, res, body) => {
+            if (!err) {
+                console.log('message sent!')
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        });
+    }
+
+    app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
